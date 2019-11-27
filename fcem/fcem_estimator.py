0656b3573565e38477465928de0dd513fab134fe
cdrobayod <cdrobayod@cyclesytem.org>
2019-11-27 17:28:43 -0500

    First Version of FCEM Threephase System

---
+++ fcem/fcem_estimator.py
@@ -0,0 +1,252 @@
+"""
+Module that condense all the components of the EMS tested above
+"""
+
+# General Imports
+
+import numpy as np
+import skfuzzy as fuzz
+
+# Sklearn imports
+
+from sklearn.base import BaseEstimator
+from sklearn.utils.validation import check_is_fitted
+import inspect
+
+class threeLevelFCEM(BaseEstimator):
+    
+    """ 
+    Fuzzy Comprehensive Evaluation Methodology implementation available to any configuration with
+    3 levels (Goals | Category | KPI).
+    
+    Parameters
+    ----------
+    kpiParameters : array (# KPIs) of matrices (5 labels x 4 points membership function), default='None' - KPI configurations
+    kpiStep : array (# KPIs), default='None' - Mapping to the step size for each KPI
+    categoryMapping : array (# KPIs), default='None' - Mapping from Category to KPI matrix using the array index
+    weightVectorCategories : array (# Categories) of arrays (# KPIs), default='None' - Weighting for Categories
+    weightVectorGoals : array (# Goals) of arrays (# Categories), default='None' - Weighting for Goals
+    """  
+    
+    def __init__(self,kpiParameters = None,kpiStep = None,categoryMapping = None,weightVectorCategories = None,categoryMapping2 = None,weightVectorSCategories = None,weightVectorGoals = None):
+        
+        # Gather all the passed parameters
+        args, _, _,values = inspect.getargvalues(inspect.currentframe())
+        
+        # Remove parameter 'self' 
+        values.pop("self")
+
+        # Init the attributes of the class
+        for arg, val in values.items():
+            setattr(self, arg, val)
+
+    def fit(self, X, y = None):
+        
+        """ 
+        Configure all the Membership Functions of the FCEM
+        
+        Features per row
+        ----------
+        X -> Not used
+        
+        Outputs
+        ----------
+        y -> Not used
+             
+        """
+        
+        # Define the functions
+        
+        self.xAxis = [];
+        self.membership = [];
+        
+        # Configure the time vector for each KPI membership function
+        
+        index = 0;
+        
+        for kpi in self.kpiParameters:
+        
+            rangeMin = min(min(kpi));
+            rangeMax = max(max(kpi));
+            
+            self.xAxis.append(np.arange(rangeMin, rangeMax, self.kpiStep[index]))
+            index += 1;
+            
+        # Create membership function array for each KPI
+            
+        index = 0;
+            
+        for kpi in self.kpiParameters:
+        
+            arrayOfMembership = [];
+        
+            for label in kpi:
+                
+                arrayOfMembership.append(fuzz.trapmf(self.xAxis[index],label));
+            
+            self.membership.append(arrayOfMembership);
+            
+            index += 1;
+            
+        
+        
+        
+        # Check input and target vectors correctness
+        # ToDo
+        
+        # Initial value for initialization flag
+        
+        self.isInitialized_ = False;
+        
+        
+        # Configure parameters when fitted
+        
+        self.isFitted_ = True;
+        
+        # `fit` should always return `self`
+        return self
+
+    def predict(self, X, y = None):
+        
+        """ 
+        Fuzzy Comprehensive Evaluation Methodology implementation available to any configuration with
+        3 levels (Goals | Category | KPI).
+        
+        Features per row
+        ----------
+        X -> List (# Desired measures) of lists (# KPIs) with processed data KPIs  
+        
+        Outputs
+        ----------
+        y[0] -> Evaluation for KPIs
+        y[1] -> Evaluation for Categories 
+        y[2] -> Evaluation for Goals 
+
+        """
+        
+        # Define the Output vector
+        
+        y = [];
+        
+        # Verification before prediction
+        
+        check_is_fitted(self, 'isFitted_')
+        
+        # Perform the prediction over the input array
+        
+        for x in X :
+            
+            result = [];
+            
+            kpiEvaluations = [];
+        
+            index = 0;
+        
+            # Evaluate all the processed data KPIs with the membership functions
+            
+            for processedKpi in x:
+                
+                evaluation = [];
+                
+                for function in self.membership[index]:
+                    
+                    evaluation.append(fuzz.interp_membership(self.xAxis[index],function,processedKpi));
+                    
+                kpiEvaluations.append(evaluation)
+                
+                index += 1;
+            
+            result.append(kpiEvaluations)
+            
+            
+            # Create lists for Category evaluation
+            
+            categoryEvaluation = [];
+            
+            for vector in self.weightVectorCategories:
+                
+                categoryEvaluation.append([]);
+            
+            # Fill the matrices of Category Evaluation
+            
+            index = 0;
+            
+            for kpi in kpiEvaluations:
+            
+                categoryEvaluation[self.categoryMapping[index]].append(kpi);
+                
+                index += 1;
+            
+            
+            # Compute the Category scores
+            
+            computedCategoryEvaluation = [];
+            
+            index = 0;
+            
+            for matrix in categoryEvaluation:
+                
+                category = np.array([self.weightVectorCategories[index]]);
+                kpis = np.array(matrix);
+                
+                computedCategoryEvaluation.append(np.dot(category,kpis)[0]);
+                
+                index += 1;
+            
+            result.append(computedCategoryEvaluation)
+            
+            # Compute the Goal Evaluation
+            ##############################################################
+            
+            categoryEvaluation2 = [];
+            
+            for vector in self.weightVectorSCategories:
+                
+                categoryEvaluation2.append([]);
+            ################################################################
+            
+            index = 0;
+            
+            for Scat in computedCategoryEvaluation:
+            
+                categoryEvaluation2[self.categoryMapping2[index]].append(Scat);
+                
+                index += 1;
+                
+           
+            ################################################################
+            
+            computedSCategoryEvaluation = [];
+            
+            index = 0;
+            
+            for matrix in categoryEvaluation2:
+                
+                Scategory = np.array([self.weightVectorSCategories[index]]);
+                Scat2 = np.array(matrix);
+                
+                computedSCategoryEvaluation.append(np.dot(Scategory,Scat2)[0]);
+                
+                index += 1;
+            
+            result.append(computedSCategoryEvaluation)
+            
+            ################################################################
+            goal = np.array(self.weightVectorGoals);
+            Scategories = computedSCategoryEvaluation;
+            
+            goalEvaluation = np.dot(goal,Scategories);
+            
+            result.append(goalEvaluation);
+        
+            y.append(result);
+        
+        # Return the vector of outputs
+        
+        return y;
+    
+    """
+    --------------------------------------------------------------------------------------------------
+    Definition of specific EMS methods - NOT NEEDED FOR NOW
+    --------------------------------------------------------------------------------------------------
+    """
